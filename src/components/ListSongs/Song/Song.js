477c65a0b04cf663e284c7b424fd29297e6d6a46
= <=>
2022-12-08 21:18:12 +0700

    'update'

--- src/components/ListSongs/Song/Song.js
+++ src/components/ListSongs/Song/Song.js
@@ -8,49 +8,51 @@ import TippyHeadless from '@tippyjs/react/headless';
 
 const cx = classNames.bind(style);
 function Song({ item }) {
-   const { handleSetSong, currentSong } = useContext(SongsApp);
-   const [activeSong, setActiveSong] = useState(0);
-   const handleActiveSong = (idSong) => {
-      handleSetSong(idSong);
-      setActiveSong(idSong);
-   };
-   const songActiveRef = useRef();
+      const { handleSetSong, currentSong } = useContext(SongsApp);
+      const [activeSong, setActiveSong] = useState(0);
+      const handleActiveSong = (idSong) => {
+            handleSetSong(idSong);
+            setActiveSong(idSong);
+      };
+      const songActiveRef = useRef();
 
-   const handelClickMore = (e) => {
-      e.stopPropagation();
-   };
+      const handelClickMore = (e) => {
+            e.stopPropagation();
+      };
 
-   useEffect(() => {
-      if (songActiveRef.current.id === currentSong.id) {
-         songActiveRef.current.scrollIntoView({ behavior: 'smooth', block: 'center' });
-      }
-      setActiveSong(currentSong.id);
-   }, [currentSong]);
+      useEffect(() => {
+            if (songActiveRef.current.id === currentSong.id) {
+                  songActiveRef.current.scrollIntoView({ behavior: 'smooth', block: 'center' });
+            }
+            setActiveSong(currentSong.id);
+      }, [currentSong]);
 
-   return (
-      <div
-         id={item.id}
-         ref={songActiveRef}
-         className={cx('wrapper-song', activeSong === item.id && 'active')}
-         onClick={() => handleActiveSong(item.id)}
-      >
-         <img className={cx('song-image')} src={item.thumbnail} alt={item.name} />
-         <p className={cx('song-name')}>{item.song_name}</p>
-         <TippyHeadless
-            interactive
-            placement={'bottom-end'}
-            render={(attrs) => (
-               <div className={cx('popper')} tabIndex="-1" {...attrs}>
-                  My tippy box
-               </div>
-            )}
-         >
-            <button className={cx('btn-more')} onClick={handelClickMore}>
-               <FontAwesomeIcon icon={faEllipsis} />
-            </button>
-         </TippyHeadless>
-      </div>
-   );
+      return (
+            <div
+                  id={item.id}
+                  ref={songActiveRef}
+                  className={cx('wrapper-song', activeSong === item.id && 'active')}
+                  onClick={() => handleActiveSong(item.id)}
+            >
+                  <div className={cx('song-image', activeSong === item.id && 'active')}>
+                        <img src={item.thumbnail} alt={item.name} />
+                  </div>
+                  <p className={cx('song-name')}>{item.song_name}</p>
+                  <TippyHeadless
+                        interactive
+                        placement={'bottom-end'}
+                        render={(attrs) => (
+                              <div className={cx('popper')} tabIndex="-1" {...attrs}>
+                                    My tippy box
+                              </div>
+                        )}
+                  >
+                        <button className={cx('btn-more')} onClick={handelClickMore}>
+                              <FontAwesomeIcon icon={faEllipsis} />
+                        </button>
+                  </TippyHeadless>
+            </div>
+      );
 }
 
 export default Song;
